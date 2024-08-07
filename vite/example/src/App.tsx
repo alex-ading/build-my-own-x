5e2b4bdc2214ff03694fdcbcca54af1fa737c981
alex-ading <1877693751@qq.com>
2024-08-07 10:35:05 +0800

    feat: vite add nodemon

--- vite/example/src/App.tsx
+++ vite/example/src/App.tsx
@@ -1,12 +1,10 @@
 import img from '../public/duck.jpg'
-// import img2 from './duck.jpg'
 
 function App() {
   return (
     <>
       <p>app</p>
       <img src={img} alt="" />
-      {/* <img src={img2} alt="" /> */}
     </>
   )
 }
